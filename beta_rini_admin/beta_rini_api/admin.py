7eb957b3898e8377f7ca79fd8c6f3282b07aceaf
Muhammad Adam Fahmil 'Ilmi <adamfahmil020@gmail.com>
2022-10-26 21:47:41 +0700

    Initiate model and Django admin

--- beta_rini_admin/beta_rini_api/admin.py
+++ beta_rini_admin/beta_rini_api/admin.py
@@ -1,3 +1,45 @@
+import logging
 from django.contrib import admin
+from beta_rini_api.models import JenisKelamin, Spesialis, Hari, Poli, \
+    Pasien, Dokter, Jadwal, Pendaftaran
+
+logger = logging.getLogger('betarini.custom')
 
 # Register your models here.
+
+@admin.register(JenisKelamin)
+class JenisKelaminAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nilai')
+
+@admin.register(Spesialis)
+class SpesialisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nilai')
+    search_fields = ('nilai',)
+
+@admin.register(Hari)
+class HariAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nilai')
+
+@admin.register(Poli)
+class PoliAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nilai')
+    search_fields = ('nilai',)
+
+@admin.register(Pasien)
+class PasienAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nomor_rm', 'nama_lengkap', 'nik', 'tanggal_dibuat', 'tanggal_diubah')
+    search_fields = ('nomor_rm', 'nama_lengkap', 'nik')
+
+@admin.register(Dokter)
+class DokterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'kode_dokter', 'nama_lengkap', 'spesialis', 'tanggal_dibuat', 'tanggal_diubah')
+    search_fields = ('kode_dokter', 'nama_lengkap')
+
+@admin.register(Jadwal)
+class JadwalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hari', 'dokter', 'waktu_mulai', 'waktu_selesai')
+
+@admin.register(Pendaftaran)
+class PendaftaranAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nomor_registrasi', 'pasien', 'poli', 'dokter', 'tanggal_registrasi')
+    search_fields = ('nomor_registrasi', 'pasien__nomor_rm', 'pasien__nama_lengkap', 'dokter__kode_dokter', 'dokter__nama_lengkap')
--- beta_rini_admin/beta_rini_api/models.py
+++ beta_rini_admin/beta_rini_api/models.py
@@ -46,6 +46,9 @@ class Pasien(models.Model):
             err_message = f'Error on clean method of Pasien model: {error}'
             logger.exception(f'{err_message} | {traceback.format_exc()}')
             raise ValidationError(err_message)
+    
+    def __str__(self):
+        return f'{self._get_pk_val()}-{self.nomor_rm}-{self.nama_lengkap}'
 
 class Spesialis(models.Model):
     nilai = models.CharField(max_length=50)
@@ -54,3 +57,54 @@ class Spesialis(models.Model):
 
     def __str__(self):
         return f'{self._get_pk_val()}-{self.nilai}'
+
+class Dokter(models.Model):
+    kode_dokter = models.CharField(max_length=10, unique=True)
+    nama_lengkap = models.CharField(max_length=100)
+    jenis_kelamin = models.ForeignKey(JenisKelamin, on_delete=models.SET_NULL, null=True, blank=True, related_name='dokter')
+    spesialis = models.ForeignKey(Spesialis, on_delete=models.SET_NULL, null=True, blank=True, related_name='dokter')
+    alamat = models.TextField(blank=True, null=True)
+    tanggal_dibuat = models.DateTimeField(auto_now_add=True)
+    tanggal_diubah = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'{self._get_pk_val()}-{self.kode_dokter}-{self.nama_lengkap}'
+
+class Hari(models.Model):
+    nilai = models.CharField(max_length=10)
+    tanggal_dibuat = models.DateTimeField(auto_now_add=True)
+    tanggal_diubah = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'{self._get_pk_val()}-{self.nilai}'
+
+class Jadwal(models.Model):
+    hari = models.ForeignKey(Hari, on_delete=models.CASCADE, related_name='jadwal')
+    dokter = models.ForeignKey(Dokter, on_delete=models.CASCADE, related_name='jadwal')
+    waktu_mulai = models.TimeField()
+    waktu_selesai = models.TimeField()
+    tanggal_dibuat = models.DateTimeField(auto_now_add=True)
+    tanggal_diubah = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'{self._get_pk_val()}-{self.hari.nilai}-{self.dokter.kode_dokter}-{self.dokter.nama_lengkap}'
+
+class Poli(models.Model):
+    nilai = models.CharField(max_length=20)
+    tanggal_dibuat = models.DateTimeField(auto_now_add=True)
+    tanggal_diubah = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'{self._get_pk_val()}-{self.nilai}'
+
+class Pendaftaran(models.Model):
+    nomor_registrasi = models.CharField(max_length=30, unique=True)
+    pasien = models.ForeignKey(Pasien, on_delete=models.CASCADE, related_name='pendaftaran')
+    poli = models.ForeignKey(Poli, on_delete=models.SET_NULL, null=True, blank=True, related_name='pendaftaran')
+    dokter = models.ForeignKey(Dokter, on_delete=models.SET_NULL, null=True, blank=True, related_name='pendaftaran')
+    tanggal_registrasi = models.DateField()
+    tanggal_dibuat = models.DateTimeField(auto_now_add=True)
+    tanggal_diubah = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'{self._get_pk_val()}-{self.nomor_registrasi}-{self.tanggal_registrasi}'
